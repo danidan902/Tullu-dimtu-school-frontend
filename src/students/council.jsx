5cac2cd4897aa6fe4404a3f8b8a3732af5970b66
Daniel Sheleme <danid93575@gmail.com>
2025-08-23 07:50:23 +0300

    Update council.jsx

--- src/students/council.jsx
+++ src/students/council.jsx
@@ -463,9 +463,9 @@ const CounselingPage = () => {
     whileHover={{ scale: 1.02 }}
     whileTap={{ scale: 0.98 }}
     className="w-full bg-gradient-to-r from-indigo-600 to-purple-600 text-white py-4 px-6 rounded-xl font-medium hover:shadow-lg transition-all duration-300 shadow-md flex items-center justify-center"
-  
+    disable={loading}
     >
-    <span>Submit Request</span>
+    <span>   {loading ? "Submit Request..." : "Submit Request" } </span>
     <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 5l7 7-7 7M5 5l7 7-7 7" />
     </svg>
