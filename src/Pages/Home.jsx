7cdab5c16ee4a3294409b63418a6e26d25bb6726
Daniel Sheleme <danid93575@gmail.com>
2025-08-23 12:46:01 +0300

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
     return (
         <div className="relative w-full font-sans antialiased">
 
-            <section className="relative w-full h-screen overflow-hidden">
+            <section className="relative  w-full h-screen overflow-hidden">
 
                 <div
                     className="absolute inset-0 bg-cover bg-center bg-fixed"
