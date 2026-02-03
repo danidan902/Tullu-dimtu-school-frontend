b3f0a7220936227c9b0e7d5e69f6a73d3aa82ef1
Daniel Sheleme <danid93575@gmail.com>
2026-02-03 15:14:09 +0300

    Update axiosConfig.js

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+const API_URL = 'https://tullu-dimtu-school-backend-1.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
